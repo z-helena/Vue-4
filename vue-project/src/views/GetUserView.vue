a08d258a07d06c03dc26bef1abb52e8532ddf51f
yzsythsbt <helena_zhang@horacemann.org>
2025-03-03 11:51:34 -0500

    commit

---
+++ vue-project/src/views/GetUserView.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                first_name: "",
+                last_name: "",
+                email: ""
+            }
+
+        }
+    },
+
+    methods: {
+        async fetchUser() {
+            const response = await fetch('https://hm-bookstore-api.onrender.com/user/3');
+            console.log(response);
+
+            const data = await response.json();
+            this.user = data;
+        }
+    }
+}
+</script>
+
+<template>
+    <p>{{ user.first_name }}</p>
+    <p>{{ user.last_name }}</p>
+    <p>{{ user.email }}</p>
+
+</template>
